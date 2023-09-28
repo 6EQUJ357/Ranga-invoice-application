94eb60f6fe11f1287e6dd444560588a79e055d55
6EQUJ357 <abhi6equj5@gmail.com>
2023-09-28 13:51:38 +0530

    build file changed...

--- client/src/pages/components/config.js
+++ client/src/pages/components/config.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = "http://localhost:8080"
+//const API_BASE_URL = "http://localhost:8080"
    
   
-//const API_BASE_URL = "https://ranga-invoice-app.onrender.com"  
+const API_BASE_URL = "https://ranga-invoice-app.onrender.com"  
  
 
 
